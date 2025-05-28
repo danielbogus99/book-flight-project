105893499f937f8c6e948ccba3828f46be7c4411
danielbogus99 <danik444@walla.co.il>
2025-05-28 18:36:18 +0300

    backend update

---
+++ backend/FlightService/src/data-access/db.js
@@ -0,0 +1,14 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, 
+});
+
+pool.connect()
+  .then(() => console.log('✅ Connected to PostgreSQL Database'))
+  .catch((err) => console.error('❌ Failed to connect to DB:', err.message));
+
+export default pool;
