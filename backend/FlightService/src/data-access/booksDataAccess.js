52f2eb5c1bd00aefc5dfe9942689ea80dc3721d8
Sapir Natanov <sapirnatanov@gmail.com>
2025-06-10 18:23:42 +0300

    added booking to the backend

---
+++ backend/FlightService/src/data-access/booksDataAccess.js
@@ -0,0 +1,23 @@
+import db from '../data-access/db.js';
+
+export const BookFlight = async ({
+    flight_id,
+    passenger_name,
+    passenger_email,
+    passenger_id,
+    total_price
+}) => {
+    try {
+        const insertQuery = `
+            INSERT INTO bookings (flight_id, passenger_name, passenger_email, passenger_id, total_price)
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING *;
+        `;
+        const values = [flight_id, passenger_name, passenger_email, passenger_id, total_price];
+        const result = await db.query(insertQuery, values);
+        return result.rows[0];
+    } catch (error) {
+        console.error('Error booking flight:', error);
+        throw error;
+    }
+}
